4221a70ffb59f54d663e327467a214aa518f5422
Kholstinin <kholstinin.da@gmail.com>
2018-03-10 17:32:56 +0500

    add tests, update and clear deps, update webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,29 +1,38 @@
-var webpack = require('webpack');
-var path = require('path');
+const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const path = require('path');
 
-var BUILD_DIR = path.resolve(__dirname, 'dist');
-var APP_DIR = path.resolve(__dirname, 'js');
-
-var config = {
-  entry: APP_DIR + '/index.jsx',
+const config = {
+  entry: './src/js/index.jsx',
   output: {
-    path: BUILD_DIR,
-    filename: 'bundle.js'
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
   },
-  module : {
-    loaders : [
+  module: {
+    rules: [
       {
-        test : /\.jsx?/,
-        include : APP_DIR,
-        loader : 'babel-loader'
+        test: /\.jsx?$/,
+        loader: 'babel-loader',
       },
       {
         test: /\.(jpg|png|svg)$/,
-        loader: 'file',
-        include: '/static/img'
-      }
-    ]
-  }
+        loader: 'file-loader',
+        options: {
+          name: 'img/[name].[ext]'
+        },
+      },
+    ],
+  },
+  devtool: 'source-map',
+  devServer: {
+    contentBase: path.join(__dirname, 'dist'),
+    port: 3000,
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: 'src/index.html'
+    })
+  ]
 };
 
 module.exports = config;
